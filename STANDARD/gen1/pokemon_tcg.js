cd730eebc503686e99f2947c75de28b5c2b9b6cd
Scotts-Thoughts <99685188+Scotts-Thoughts@users.noreply.github.com>
2025-04-01 16:26:15 -0600

    char map, owned cards
    char map
    owned cards
    player name
    state management
    opponent duel values
    medals
    grandmasters
    event vars - although these have errors
    maps
    overworld, gametime, audio
    music track names

--- STANDARD/gen1/pokemon_tcg.js
+++ STANDARD/gen1/pokemon_tcg.js
@@ -51,7 +51,22 @@ function setProperty(path, values) {
     if (values.value !== undefined)
         property.value = values.value;
 }
-
+function getGameState() {
+    const music_track = getValue(`audio.current_song`)
+    switch (music_track) {
+        case "MUSIC_MATCH_START_1": return "To Battle";
+        case "MUSIC_MATCH_START_2": return "To Battle";
+        case "MUSIC_MATCH_START_3": return "To Battle";
+        case "MUSIC_DUEL_THEME_1" : return "Battle";
+        case "MUSIC_DUEL_THEME_2" : return "Battle";
+        case "MUSIC_DUEL_THEME_3" : return "Battle";
+        case "MUSIC_MATCH_VICTORY": return "From Battle";
+        case "MUSIC_MATCH_LOSS"   : return "From Battle";
+        case "MUSIC_MATCH_DRAW"   : return "From Battle";
+        default:
+            return "Overworld";
+    }
+}
 function getHand(cards_in_hand, raw_hand, hand_path, deck_path) {
     // Get the number of cards in the player's hand.
     const numCards = getValue(cards_in_hand);
@@ -71,25 +86,27 @@ function getHand(cards_in_hand, raw_hand, hand_path, deck_path) {
     }
 }
 function setIndirectReference(set_location, indirect_reference_path, value_path) {
+    if (getValue(indirect_reference_path) == 255) { return }
     setValue(set_location, getValue(`${value_path}.${getValue(indirect_reference_path)}`));
 }
 
+// Post Processor
 function postprocessor() {
     // Set both Arena Pokemon
-    if (getValue(`player.wPlayerArenaCard`) != 0) { setIndirectReference(`player.arena_pokemon`, `player.wPlayerArenaCard`, `player.deck_1`); }
+    if (getValue(`player.wPlayerArenaCard`) != 0) { setIndirectReference(`player.arena_pokemon`, `player.wPlayerArenaCard`, `player.deck`); }
     if (getValue(`player.wPlayerArenaCard`) != 0) { setIndirectReference(`opponent.arena_pokemon`, `opponent.wOpponentArenaCard`, `opponent.deck`); }
     
     // Populate hands with the correct values
-    getHand("player.wPlayerNumberOfCardsInHand", "player.hand_raw", "player.hand", "player.deck_1")
+    getHand("player.wPlayerNumberOfCardsInHand", "player.hand_raw", "player.hand", "player.deck")
     getHand("opponent.wOpponentNumberOfCardsInHand", "opponent.hand_raw", "opponent.hand", "opponent.deck")
 
     // Set player bench Pokemon
-    if (getValue(`player.bench_raw.0`) != 255) { setIndirectReference(`player.bench.0`, `player.bench_raw.0`, `player.deck_1`); }
-    if (getValue(`player.bench_raw.1`) != 255) { setIndirectReference(`player.bench.1`, `player.bench_raw.1`, `player.deck_1`); }
-    if (getValue(`player.bench_raw.2`) != 255) { setIndirectReference(`player.bench.2`, `player.bench_raw.2`, `player.deck_1`); }
-    if (getValue(`player.bench_raw.3`) != 255) { setIndirectReference(`player.bench.3`, `player.bench_raw.3`, `player.deck_1`); }
-    if (getValue(`player.bench_raw.4`) != 255) { setIndirectReference(`player.bench.4`, `player.bench_raw.4`, `player.deck_1`); }
-    if (getValue(`player.bench_raw.5`) != 255) { setIndirectReference(`player.bench.5`, `player.bench_raw.5`, `player.deck_1`); }
+    if (getValue(`player.bench_raw.0`) != 255) { setIndirectReference(`player.bench.0`, `player.bench_raw.0`, `player.deck`); }
+    if (getValue(`player.bench_raw.1`) != 255) { setIndirectReference(`player.bench.1`, `player.bench_raw.1`, `player.deck`); }
+    if (getValue(`player.bench_raw.2`) != 255) { setIndirectReference(`player.bench.2`, `player.bench_raw.2`, `player.deck`); }
+    if (getValue(`player.bench_raw.3`) != 255) { setIndirectReference(`player.bench.3`, `player.bench_raw.3`, `player.deck`); }
+    if (getValue(`player.bench_raw.4`) != 255) { setIndirectReference(`player.bench.4`, `player.bench_raw.4`, `player.deck`); }
+    if (getValue(`player.bench_raw.5`) != 255) { setIndirectReference(`player.bench.5`, `player.bench_raw.5`, `player.deck`); }
 
     // Set opponent bench Pokemon
     if (getValue(`opponent.bench_raw.0`) != 255) { setIndirectReference(`opponent.bench.0`, `opponent.bench_raw.0`, `opponent.deck`); }
@@ -98,6 +115,9 @@ function postprocessor() {
     if (getValue(`opponent.bench_raw.3`) != 255) { setIndirectReference(`opponent.bench.3`, `opponent.bench_raw.3`, `opponent.deck`); }
     if (getValue(`opponent.bench_raw.4`) != 255) { setIndirectReference(`opponent.bench.4`, `opponent.bench_raw.4`, `opponent.deck`); }
     if (getValue(`opponent.bench_raw.5`) != 255) { setIndirectReference(`opponent.bench.5`, `opponent.bench_raw.5`, `opponent.deck`); }
+
+    // Update the game state
+    setValue(`meta.state`, getGameState());
 }
 
 export { postprocessor };
