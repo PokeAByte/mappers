29c0186e9f55f37cd806b495eb68d8dcba0eb1c7
PokeAByte <PokeAByte@users.noreply.github.com>
2026-01-24 15:30:11 +0000

    Updated 9 mappers
    STANDARD/gen3/pokemon_firered_leafgreen.xml: 1.1.0 -> 1.1.1
    STANDARD/gen3/pokemon_emerald.xml: 1.1.0 -> 1.1.1
    STANDARD/gen3/pokemon_ruby_sapphire.xml: 1.1.0 -> 1.1.1
    STANDARD/gen4/pokemon_diamond_pearl.xml: 0.3.2 -> 0.3.3
    STANDARD/gen4/pokemon_platinum.xml: 1.1.1 -> 1.1.2
    STANDARD/gen4/pokemon_heartgold_soulsilver.xml: 0.3.2 -> 0.3.3
    STANDARD/gen5/pokemon_black.xml: 0.8.1 -> 0.8.2
    STANDARD/gen5/pokemon_white.xml: 0.7.11 -> 0.7.12
    DEPRECATED/gen1/pokemon_yellow_precated.xml: new -> 0.1.0

--- STANDARD/gen3/pokemon_ruby_sapphire.js
+++ STANDARD/gen3/pokemon_ruby_sapphire.js
@@ -1,3 +1,4 @@
+import { pokemon } from "game_functions";
 // @ts-ignore
 const variables = __variables;
 // @ts-ignore
@@ -8,22 +9,10 @@ const memory = __memory;
 const mapper = __mapper;
 // @ts-ignore
 __console;
-function getValue(path) {
-    // @ts-ignore
-    const property = mapper.properties[path];
-    if (!property) {
-        throw new Error(`${path} is not defined in properties.`);
-    }
-    return property.value;
-}
-function setValue(path, value) {
-    // @ts-ignore
-    const property = mapper.properties[path];
-    if (!property) {
-        throw new Error(`${path} is not defined in properties.`);
-    }
-    property.value = value;
-}
+const getValue = mapper.get_property_value;
+const setValue = mapper.set_property_value;
+const copyProperties = mapper.copy_properties;
+
 function getProperty(path) {
     // @ts-ignore
     const property = mapper.properties[path];
@@ -52,55 +41,6 @@ function setProperty(path, values) {
         property.value = values.value;
 }
 
-function copyProperties(sourcePath, destinationPath) {
-	const destPathLength = destinationPath.length;
-	Object.keys(mapper.properties)
-		.filter(key => key.startsWith(destinationPath))
-		.forEach((key) => {
-			const source = mapper.properties[`${sourcePath}${key.slice(destPathLength)}`];
-			if (source) {
-				setProperty(key, source);
-			}
-		});
-}
-
-function DATA32_LE(data, offset) {
-    let val = (data[offset] << 0)
-        | (data[offset + 1] << 8)
-        | (data[offset + 2] << 16)
-        | (data[offset + 3] << 24);
-    return val >>> 0;
-}
-// Block shuffling orders - used for Party structure encryption and decryption
-// Once a Pokemon's data has been generated it is assigned a PID which determines the order of the blocks
-// As the Pokemon's PID never changes, the order of the blocks always remains the same for that Pokemon
-// Each individial Pokemon receives its own unique shuffle order
-const shuffleOrders = {
-    0:  [0, 1, 2, 3],
-    1:  [0, 1, 3, 2],
-    2:  [0, 2, 1, 3],
-    3:  [0, 3, 1, 2],
-    4:  [0, 2, 3, 1],
-    5:  [0, 3, 2, 1],
-    6:  [1, 0, 2, 3],
-    7:  [1, 0, 3, 2],
-    8:  [2, 0, 1, 3],
-    9:  [3, 0, 1, 2],
-    10: [2, 0, 3, 1],
-    11: [3, 0, 2, 1],
-    12: [1, 2, 0, 3],
-    13: [1, 3, 0, 2],
-    14: [2, 1, 0, 3],
-    15: [3, 1, 0, 2],
-    16: [2, 3, 0, 1],
-    17: [3, 2, 0, 1],
-    18: [1, 2, 3, 0],
-    19: [1, 3, 2, 0],
-    20: [2, 1, 3, 0],
-    21: [3, 1, 2, 0],
-    22: [2, 3, 1, 0],
-    23: [3, 2, 1, 0]
-};
 const hidden_power_types = {
     0 : "Fighting",
     1 : "Flying",
@@ -241,38 +181,7 @@ function preprocessor() {
                 startingAddress = 0x30045C0 + (100 * slotIndex);
             }
             let pokemonData = memory.defaultNamespace.get_bytes(startingAddress, 100);
-            let pid = pokemonData.get_uint32_le();
-            let ot_id = pokemonData.get_uint32_le(4);
-            let decryptedData = [];
-            for (let i = 0; i < 100; i++) { //Transfer the first 32-bytes of unencrypted data to the decrypted data array
-                decryptedData[i] = pokemonData.data[i];
-            }
-            //Begin the decryption process for the block data
-            let key = ot_id ^ pid; //Calculate the encryption key using the Oritinal Trainer ID XODed with the PID
-            for (let i = 32; i < 80; i += 4) {
-                let data = DATA32_LE(pokemonData.data, i) ^ key; //XOR the data with the key
-                decryptedData[i + 0] = data & 0xFF; // Isolates the least significant byte
-                decryptedData[i + 1] = (data >> 8) & 0xFF; // Isolates the 2nd least significant byte
-                decryptedData[i + 2] = (data >> 16) & 0xFF; // Isolates the 3rd least significant byte
-                decryptedData[i + 3] = (data >> 24) & 0xFF; // Isolates the most significant byte
-            }
-            //Determine how the block data is shuffled   
-            const shuffleId = pid % 24; //Determine the shuffle order index
-            let shuffleOrder = shuffleOrders[shuffleId]; //Recall the shuffle order
-            if (!shuffleOrder) {
-                throw new Error("The PID returned an unknown substructure order.");
-            }
-            let dataCopy = Array.from(decryptedData); // Initialize a copy of the decrypted data (48-bytes only)
-            decryptedData = Array.from(decryptedData);
-            dataCopy = dataCopy.splice(32, 48);
-            //Unshuffle the block data
-            for (let i = 0; i < 4; i++) { // Copy the shuffled blocks into the decryptedData
-                decryptedData.splice(32 + i * 12, 12, ...dataCopy.slice(shuffleOrder[i] * 12, shuffleOrder[i] * 12 + 12));
-            }
-            //Transfer the remaining 20-bytes of unencrypted data to the decrypted data array
-            for (let i = 80; i < 100; i++) {
-                decryptedData[i] = pokemonData.data[i];
-            }
+            let decryptedData = pokemon.Decrypt(3, pokemonData.data);    
             //Fills the memory contains for the mapper's class to interpret
             memory.fill(`${user}_party_structure_${slotIndex}`, 0x00, decryptedData);
         }
